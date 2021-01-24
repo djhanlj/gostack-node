9e1e0620d7508fc0d73c37825c671c2a8021930e
djair <djhanlj@gmail.com>
2021-01-24 12:25:08 -0300

    refatoração da entidade Appointments para utilizar typeorm

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,16 @@
-import { uuid } from 'uuidv4'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appoinments')
 class Appointment
 {
+  @PrimaryGeneratedColumn('uuid')
   id: String;
+
+  @Column()
   provider: String;
+
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({provider, date } : Omit<Appointment, 'id'> ){
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
-
 }
 
 export default Appointment;
