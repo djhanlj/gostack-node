7a18f83b3f3eabffb44639e9a45878720b72657b
djair <djhanlj@gmail.com>
2021-01-30 17:37:29 -0300

    finalização do upload de imagens

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { request, response } from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import 'reflect-metadata'
 import './database'
@@ -7,6 +8,7 @@ import './database'
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes)
 
 app.get('/', (request, response) => {
